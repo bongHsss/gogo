e19e27150bf1a0fc5c7fc159fbcf9335e6ed069f
bongHsss <dev_bhs@naver.com>
2024-05-05 00:31:28 +0900

    내가 서버에 접속할 수 있게 127.0.0.1 추가

--- pro1/pro1/settings.py
+++ pro1/pro1/settings.py
@@ -131,3 +131,4 @@ STATICFILES_DIRS = [BASE_DIR / 'boots/static',]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
